13b917bb53d27fd562c7e738dd11538b7c33e0b2
mehdi <mehdiasadi63@gmail.com>
2026-02-16 12:39:01 +0330

    update bookmark

--- src/app/bookmarked/page.jsx
+++ src/app/bookmarked/page.jsx
@@ -1,14 +1,17 @@
 'use client'
 import ContentPage from '../components/ContentPage'
+import { Suspense } from 'react'
 
 export default function BookmarkedPage() {
 	return (
-		<ContentPage
-			title="Bookmarked"
-			placeholder="Search for bookmarked shows"
-			filterFn={item => item.isBookmarked}
-			showTrending={false}
-			showRecommended={false}
-		/>
+		<Suspense fallback={<div>Loading...</div>}>
+			<ContentPage
+				title="Bookmarked"
+				placeholder="Search for bookmarked shows"
+				filterFn={item => item.isBookmarked}
+				showTrending={false}
+				showRecommended={false}
+			/>
+		</Suspense>
 	)
 }
